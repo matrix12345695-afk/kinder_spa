f194c66cc93625f7f04744d2acc2e8bad25bbb63
matrix12345695-afk <matrix12345695@gmail.com>
2026-04-16 12:25:00 +0500

    Delete sheets directory

--- sheets/__init__.py
+++
@@ -1,4 +0,0 @@
-
-def get_active_masses(lang): return [{"id":1,"name":"Массаж","price":"100000"}]
-def get_therapists_for_massage(id): return [{"id":1,"name":"Анна"}]
-def create_appointment(*args): pass
