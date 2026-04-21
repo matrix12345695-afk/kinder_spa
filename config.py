f154adca9a53bc3b03ce1d9534cde24966b0d7e2
matrix12345695-afk <matrix12345695@gmail.com>
2026-04-21 17:54:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 OPERATOR_ID = int(os.getenv("OPERATOR_ID", "0"))
+
+# 👇 ВОТ ЭТО ДОБАВЬ
+GOOGLE_SHEET_NAME = os.getenv("SPREADSHEET_NAME", "kinder_spa")
+GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS", "credentials.json")
