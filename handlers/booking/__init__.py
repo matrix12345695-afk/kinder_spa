cf4ba77d2b94dbed6c5a007944f0a7d377d3aee5
matrix12345695-afk <matrix12345695@gmail.com>
2026-04-21 17:18:52 +0500

    Update __init__.py

--- handlers/booking/__init__.py
+++ handlers/booking/__init__.py
@@ -1,7 +1,10 @@
-
 from aiogram import Router
+
 from .start import router as start_router
 from .form import router as form_router
+
+# 💥 ДОБАВЛЯЕМ ЭКСПОРТ ФУНКЦИИ
+from .booking import start_booking
 
 router = Router()
 router.include_router(start_router)
